22c937ab707602cfec019ea2c2eb08c0218c4f2c
dkleidonaris <dkleidonaris@gmail.com>
2025-05-06 02:46:28 +0300

    add default admin address

--- config/mail.php
+++ config/mail.php
@@ -130,6 +130,5 @@ return [
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
-    'admin_address' => env('ADMIN_MAIL')
-
+    'admin_address' => env('MAIL_ADMIN_ADDRESS', 'info@filidas.gr')
 ];
