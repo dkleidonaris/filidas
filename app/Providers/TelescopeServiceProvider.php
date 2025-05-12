b4b6d860b575ab69d32cf0aacfee3479dbe0a0f6
Dimitris Kleidonaris <dkleidonaris@gmail.com>
2025-05-12 12:00:25 +0300

    allowed dkleidonaris to view telescope

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -60,7 +60,7 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
     {
         Gate::define('viewTelescope', function ($user) {
             return in_array($user->email, [
-                'd@d.com'
+                'dkleidonaris@gmail.com'
             ]);
         });
     }
